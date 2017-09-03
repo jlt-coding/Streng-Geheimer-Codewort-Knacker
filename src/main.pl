d21addf50fe4183aac0a065e8cda8371fd26616e
JLT-Coding <30775510+jlt-coding@users.noreply.github.com>
2017-09-03 20:16:23 +0200

    [KE] final changes to main.pl
    added some comments, did QA, formatted output, etc...

--- src/main.pl
+++ src/main.pl
@@ -12,36 +12,38 @@ my @letterlist;
 # Read the inputfile
 open (my $fh_input, '<', $data_path . '/final.txt') or
 	die "Cannot open file, $!.";
-	
-	
+		
 while (my $line = <$fh_input>) {
 	chomp $line;
 	push(@word_list, $line);
 }
 
 close $fh_input;
+
 for my $i (1 .. 5) {
 	$lettercount -= 1;
-	@word_list = solution_finder(\@word_list, $lettercount);
+	@word_list = solution_finder(\@word_list, $lettercount); # Pass @word_list as a reference
 }
-print @word_list;
+
+print "This is a list of possible solutions for todays case:\n@word_list\nBleibt auf der Spur!";
 
 sub solution_finder{ # \@word_list, $lettercount
 	my ($word, $count) = @_;
-	my @words = @{$word};
+	my @words = @{$word}; # Dereference @word_list for use in function
 	my @rest_of_list;
 	print "Please enter the possible solutions for the current riddle, delimited by a comma after every letter.
 (especially the last one e.g.: a,c,h,t,b,).
 Alternatively, type [count] without brackets to get the number of remaining possible solutions.\n";
-
 	$input = <STDIN>;
 	chomp $input;
 	$input = lc($input);
 	
+	# Determine whether the input is an acceptable list of letters by checking if the second character is a comma.
+	# This is probably not the most elegant way to verify the input
 	while (substr($input, 1, 1) ne ',') {
 		if ($input eq 'count') {
-			print scalar(@word_list) . "\n";
-			print "Please enter now the things:\n";
+			print scalar(@word_list) . " remaining possible solutions.\n";
+			print "Please enter now the solutions for the current riddle:\n";
 			$input = <STDIN>;
 		} else {
 			print "This input is incorrect, please try again";
@@ -50,8 +52,10 @@ Alternatively, type [count] without brackets to get the number of remaining poss
 		}
 	}
 	
+	# For every letter in the submitted solution, check every word in the supplied word list for the presence of this letter on the current position in the word.
+	# Store all suitable words in a new array. This array is the new @word_list
 	my @letters = split (/,/, $input);
-	foreach my $buchstabe (@letters) {;
+	foreach my $buchstabe (@letters) {
 		foreach my $wort (@words) {
 			$wort = lc($wort);
 			if (substr($wort, $count, 1) eq $buchstabe) {
